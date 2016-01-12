eed13b2a30db511f4f5a584b9f09a35b58098145
rizar <dimabv@tut.by>
2016-01-12 11:27:49 -0500

    Use Synchronize extension

--- mnist/__init__.py
+++ mnist/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import logging
+import numpy
 from argparse import ArgumentParser
 
 from theano import tensor
@@ -12,7 +13,7 @@ from blocks.initialization import IsotropicGaussian, Constant
 from fuel.streams import DataStream
 from fuel.transformers import Flatten
 from fuel.datasets import MNIST
-from fuel.schemes import SequentialScheme
+from fuel.schemes import ShuffledScheme
 from blocks.filter import VariableFilter
 from blocks.graph import ComputationGraph
 from blocks.model import Model
@@ -22,6 +23,8 @@ from blocks.extensions.saveload import Checkpoint
 from blocks.extensions.monitoring import (DataStreamMonitoring,
                                           TrainingDataMonitoring)
 from blocks.main_loop import MainLoop
+from blocks_extras.extensions.synchronization import Synchronize
+from platoon.param_sync import EASGD
 
 try:
     from blocks.extras.extensions.plot import Plot
@@ -30,7 +33,7 @@ except:
     BLOCKS_EXTRAS_AVAILABLE = False
 
 
-def main(save_to, num_epochs):
+def main(save_to, seed, num_epochs, sync_freq, alpha):
     mlp = MLP([Tanh(), Softmax()], [784, 100, 10],
               weights_init=IsotropicGaussian(0.01),
               biases_init=Constant(0))
@@ -52,24 +55,28 @@ def main(save_to, num_epochs):
     algorithm = GradientDescent(
         cost=cost, parameters=cg.parameters,
         step_rule=Scale(learning_rate=0.1))
-    extensions = [Timing(),
-                  FinishAfter(after_n_epochs=num_epochs),
-                  DataStreamMonitoring(
-                      [cost, error_rate],
-                      Flatten(
-                          DataStream.default_stream(
-                              mnist_test,
-                              iteration_scheme=SequentialScheme(
-                                  mnist_test.num_examples, 500)),
-                          which_sources=('features',)),
-                      prefix="test"),
-                  TrainingDataMonitoring(
-                      [cost, error_rate,
-                       aggregation.mean(algorithm.total_gradient_norm)],
-                      prefix="train",
-                      after_epoch=True),
-                  Checkpoint(save_to),
-                  Printing()]
+    extensions = [Timing()]
+    if alpha:
+        extensions += [Synchronize('MNIST', 1111, EASGD(alpha),
+                                   every_n_batches=sync_freq)]
+    extensions += [
+        FinishAfter(after_n_epochs=num_epochs),
+        DataStreamMonitoring(
+            [cost, error_rate],
+        Flatten(
+            DataStream.default_stream(
+                mnist_test,
+                iteration_scheme=ShuffledScheme(
+                    mnist_test.num_examples, 500)),
+            which_sources=('features',)),
+            prefix="test"),
+        TrainingDataMonitoring(
+            [cost, error_rate,
+             aggregation.mean(algorithm.total_gradient_norm)],
+            prefix="train",
+            after_epoch=True),
+        Checkpoint(save_to),
+        Printing()]
 
     if BLOCKS_EXTRAS_AVAILABLE:
         extensions.append(Plot(
@@ -84,8 +91,9 @@ def main(save_to, num_epochs):
         Flatten(
             DataStream.default_stream(
                 mnist_train,
-                iteration_scheme=SequentialScheme(
-                    mnist_train.num_examples, 50)),
+                iteration_scheme=ShuffledScheme(
+                    mnist_train.num_examples, 50,
+                    rng=numpy.random.RandomState(seed))),
             which_sources=('features',)),
         model=Model(cost),
         extensions=extensions)
@@ -93,13 +101,21 @@ def main(save_to, num_epochs):
     main_loop.run()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     parser = ArgumentParser("An example of training an MLP on"
                             " the MNIST dataset.")
     parser.add_argument("--num-epochs", type=int, default=2,
                         help="Number of training epochs to do.")
+    parser.add_argument("--seed", type=int, default=1,
+                        help="Random seed for data traversal")
+    parser.add_argument("--sync-freq", type=int, default=10,
+                        help="Synchronization frequency")
+    parser.add_argument("--alpha", type=float, default=None,
+                        help="Alpha parameter for EASGD. If given"
+                             " training process is synchronized with"
+                             " similar instances.")
     parser.add_argument("save_to", default="mnist.pkl", nargs="?",
                         help=("Destination to save the state of the training "
                               "process."))
     args = parser.parse_args()
-    main(args.save_to, args.num_epochs)
+    main(args.save_to, args.seed, args.num_epochs, args.sync_freq, args.alpha)
