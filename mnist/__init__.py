b5e3990a07bbf8a47442adc7bbb23e36693f67bf
rizar <dimabv@tut.by>
2016-06-06 15:06:57 -0400

    Configurable scale and momentum

--- mnist/__init__.py
+++ mnist/__init__.py
@@ -5,15 +5,14 @@ from argparse import ArgumentParser
 
 from theano import tensor
 
-from blocks.algorithms import GradientDescent, Scale
-from blocks.bricks import MLP, Tanh, Softmax
+from blocks.algorithms import GradientDescent, Momentum
+from blocks.bricks import MLP, Softmax, Rectifier
 from blocks.bricks.cost import CategoricalCrossEntropy, MisclassificationRate
 from blocks.initialization import IsotropicGaussian, Constant
 from fuel.streams import DataStream
 from fuel.transformers import Flatten
 from fuel.datasets import MNIST
 from fuel.schemes import SequentialScheme
-from blocks.filter import VariableFilter
 from blocks.graph import ComputationGraph
 from blocks.model import Model
 from blocks.monitoring import aggregation
@@ -22,7 +21,6 @@ from blocks.extensions.saveload import Checkpoint
 from blocks.extensions.monitoring import (DataStreamMonitoring,
                                           TrainingDataMonitoring)
 from blocks.main_loop import MainLoop
-from blocks.roles import WEIGHT
 
 try:
     from blocks_extras.extensions.plot import Plot
@@ -31,8 +29,8 @@ except:
     BLOCKS_EXTRAS_AVAILABLE = False
 
 
-def main(save_to, num_epochs):
-    mlp = MLP([Tanh(), Softmax()], [784, 100, 10],
+def main(save_to, scale, momentum, num_epochs):
+    mlp = MLP([Rectifier(), Rectifier(), Softmax()], [784, 1000, 1000, 10],
               weights_init=IsotropicGaussian(0.01),
               biases_init=Constant(0))
     mlp.initialize()
@@ -41,10 +39,7 @@ def main(save_to, num_epochs):
     probs = mlp.apply(x)
     cost = CategoricalCrossEntropy().apply(y.flatten(), probs)
     error_rate = MisclassificationRate().apply(y.flatten(), probs)
-
-    cg = ComputationGraph([cost])
-    W1, W2 = VariableFilter(roles=[WEIGHT])(cg.variables)
-    cost = cost + .00005 * (W1 ** 2).sum() + .00005 * (W2 ** 2).sum()
+    cg = ComputationGraph(cost)
     cost.name = 'final_cost'
 
     mnist_train = MNIST(("train",))
@@ -52,7 +47,7 @@ def main(save_to, num_epochs):
 
     algorithm = GradientDescent(
         cost=cost, parameters=cg.parameters,
-        step_rule=Scale(learning_rate=0.1))
+        step_rule=Momentum(learning_rate=scale, momentum=momentum))
     extensions = [Timing(),
                   FinishAfter(after_n_epochs=num_epochs),
                   DataStreamMonitoring(
