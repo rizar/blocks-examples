82c1e9bb4202a23447c7873f7eb699b250958b9b
rizar <dimabv@tut.by>
2016-06-06 15:43:44 -0400

    Add forgotten main file

---
+++ mnist/__main__.py
@@ -0,0 +1,20 @@
+import logging
+from argparse import ArgumentParser
+
+from mnist import main
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    parser = ArgumentParser("An example of training an MLP on"
+                            " the MNIST dataset.")
+    parser.add_argument("--num-epochs", type=int, default=2,
+                        help="Number of training epochs to do.")
+    parser.add_argument("--scale", type=float,
+                        help="Scale")
+    parser.add_argument("--momentum", type=float,
+                        help="Momentum")
+    parser.add_argument("save_to", default="mnist.pkl", nargs="?",
+                        help=("Destination to save the state of the training "
+                              "process."))
+    args = parser.parse_args()
+    main(**args.__dict__)
